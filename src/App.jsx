8bd363c38418f4317c98c3dec23288e219df9cc0
Your Name <your-email@example.com>
2024-05-20 19:23:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';  
+import Netflix from './pages/Netflix';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Player from './pages/Player';
+
+const App = () => {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/player" element={<Player />} />
+    <Route path="/" element={<Netflix />} />
+   </Routes>   
+  </BrowserRouter> );
+};
+
+export default App;
