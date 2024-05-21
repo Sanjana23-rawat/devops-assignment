3a5e869ff5f1964f72d6b209faed4864dd712381
Your Name <your-email@example.com>
2024-05-21 16:48:52 +0530

    TVshows

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Netflix from './pages/Netflix';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Player from './pages/Player';
+import Movies from './pages/Movies';
+import TVShows from './pages/TVShow';
 
 
 
@@ -14,6 +16,8 @@ const App = () => {
     <Route path="/login" element={<Login />} />
     <Route path="/signup" element={<Signup />} />
     <Route path="/player" element={<Player />} />
+    <Route path="/movies" element={<Movies />} />
+    <Route path="/tv" element={<TVShows />} />
     <Route path="/" element={<Netflix />} />
    </Routes>   
   </BrowserRouter> );
